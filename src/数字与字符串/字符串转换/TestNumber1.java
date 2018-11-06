1ffa2dc8ad0e08820b281495ec73191e5bcca746
JXY3511088jxy <765101769@qq.com>
2018-11-06 11:06:32 +0800

    时间：20181106

---
+++ src/数字与字符串/字符串转换/TestNumber1.java
@@ -0,0 +1,32 @@
+package 数字与字符串.字符串转换;
+
+public class TestNumber1 {
+    public static void main(String[] args) {
+        /*
+        数字转字符串
+        方法1： 使用String类的静态方法valueOf
+        方法2： 先把基本类型装箱为对象，然后调用对象的toString
+         */
+        int i = 5;
+
+        //方法1
+        String str = String.valueOf(i);
+
+        //方法2
+        Integer it = i;
+        String str1 = it.toString();
+        System.out.println(str1);
+
+        /*
+        字符串转数字
+        调用Integer的静态方法parseInt
+         */
+
+        String str2 = "999";
+
+        int i1 = Integer.parseInt(str2);
+
+        System.out.println(i1);
+
+    }
+}
---
+++ src/数字与字符串/格式化输出/TestNumber3.java
@@ -0,0 +1,52 @@
+package 数字与字符串.格式化输出;
+
+import java.util.Locale;
+
+public class TestNumber3 {
+
+    public static void main(String[] args) {
+
+        String name ="盖伦";
+        int kill = 8;
+        String title="超神";
+
+        /*
+        如果不使用格式化输出，就需要进行字符串连接，如果变量比较多，拼接就会显得繁琐
+        使用格式化输出，就可以简洁明了
+         */
+
+        //直接使用+进行字符串连接，编码感觉会比较繁琐，并且维护性差,易读性差
+        String sentence = name+ " 在进行了连续 " + kill + " 次击杀后，获得了 " + title +" 的称号";
+        System.out.println(sentence);
+
+        //使用格式化输出
+        //%s表示字符串，%d表示数字,%n表示换行
+        String sentenceFormat ="%s 在进行了连续 %d 次击杀后，获得了 %s 的称号%n";
+        System.out.printf(sentenceFormat,name,kill,title);
+
+        //使用format格式化输出
+        System.out.format(sentenceFormat,name,kill,title);
+
+        int year = 2020;
+        //总长度，左对齐，补0，千位分隔符，小数点位数，本地化表达
+
+        //直接打印数字
+        System.out.format("%d%n",year);
+        //总长度是8,默认右对齐
+        System.out.format("%8d%n",year);
+        //总长度是8,左对齐
+        System.out.format("%-8d%n",year);
+        //总长度是8,不够补0
+        System.out.format("%08d%n",year);
+        //千位分隔符
+        System.out.format("%,8d%n",year*10000);
+
+        //小数点位数
+        System.out.format("%.2f%n",Math.PI);
+
+        //不同国家的千位分隔符
+        System.out.format(Locale.FRANCE,"%,.2f%n",Math.PI*10000);
+        System.out.format(Locale.US,"%,.2f%n",Math.PI*10000);
+        System.out.format(Locale.UK,"%,.2f%n",Math.PI*10000);
+    }
+}
